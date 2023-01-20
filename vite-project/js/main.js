72fbcde2e64516922a256da58e1ec48b3d468480
anishaparvez <113203774+anishaparvez@users.noreply.github.com>
2023-01-20 14:31:03 -0500

    as

---
+++ vite-project/js/main.js
@@ -0,0 +1,110 @@
+import "./style.css";
+const Menu = [
+  {
+    Name: "6 Assorted Sushi Rolls (8 pieces each roll)",
+    IMG: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fproperfoodie.com%2Fwp-content%2Fuploads%2F2019%2F06%2Fsushi-rolls-hero-1.jpg&imgrefurl=https%3A%2F%2Fproperfoodie.com%2Fsushi-rolls%2F&tbnid=oDl4L1_VgCkG6M&vet=12ahUKEwjF7L_OrYb8AhUAi3IEHZIXD54QMygEegUIARDVAQ..i&docid=DA1en57T3BwRXM&w=796&h=796&q=5%20sushi%20rolls%20pictures&ved=2ahUKEwjF7L_OrYb8AhUAi3IEHZIXD54QMygEegUIARDVAQ ",
+    Side: "Kani Salad",
+    Price: "$45.99",
+  },
+  {
+    Name: "Surf and Turf",
+    IMG: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F636514364%2Fphoto%2Ffilet-mignon-steak-with-lobster-tail-surf-and-turf-meal.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3DV8aClmTWvgZGc7-RUcFy0Cn2s51y1U_WGNvugrznDfY%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fsurf-and-turf&tbnid=sbJopKyNp6zTNM&vet=12ahUKEwjPzaqArob8AhU2p3IEHdSEC6YQMygAegUIARDRAQ..i&docid=HTo7oye1yt_K8M&w=612&h=436&q=surf%20and%20turf%20pictures%20professional&ved=2ahUKEwjPzaqArob8AhU2p3IEHdSEC6YQMygAegUIARDRAQ ",
+    Side: "Mashed Potatoes",
+    Price: "$47.99",
+  },
+  {
+    Name: "Rice Lamb Shank",
+    IMG: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fthumbs.dreamstime.com%2Fb%2Fpopular-yemeni-baked-lamb-shank-served-basmati-rice-close-up-frying-pan-horizontal-table-205703606.jpg&imgrefurl=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Flamb-shank-rice.html&tbnid=gUeH2YyPETOaIM&vet=12ahUKEwj6lu22rob8AhWSn3IEHVG4AzkQMygBegUIARDTAQ..i&docid=vHqBWZ-z6xZIhM&w=800&h=533&q=rice%20lamb%20shankpictures%20professional&ved=2ahUKEwj6lu22rob8AhWSn3IEHVG4AzkQMygBegUIARDTAQ ",
+    Side: "Salad",
+    Price: "$42.99",
+  },
+  {
+    Name: "Chicken over rice ",
+    IMG: "https://static01.nyt.com/images/2015/12/21/dining/21COOKING_CHICKENnONION2/21COOKING_CHICKENnONION2-superJumbo.jpg",
+    Side: "Lentil Soup",
+    Price: "$36.99",
+  },
+  {
+    Name: "Pasta Ala Bolognese",
+    IMG: "https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_150,q_auto:low,fl_lossy,dpr_2.0,c_fill,f_auto,h_130,g_auto/m7z63ak8dclxrau3ks",
+    Side: "Garlic bread",
+    Price: "$20.99",
+  },
+  {
+    Name: "Branizi Fillet with Rice",
+    IMG: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fik.imagekit.io%2Fmunchery%2Frecipes%2Ftr%3Aw-320%2Fpan-seared-branzino.jpeg&imgrefurl=https%3A%2F%2Fwww.munchery.com%2Frecipes%2Ffish-beautiful-fish%2Fpan-seared-branzino%2F&tbnid=u808YTDh9d9DkM&vet=12ahUKEwif2tLK7Yj8AhUcs3IEHWwGBHsQMygAegUIARDWAQ..i&docid=xHdy1VusmA10AM&w=320&h=320&q=Branzino%20Fillet%20with%20Rice%20professional&ved=2ahUKEwif2tLK7Yj8AhUcs3IEHWwGBHsQMygAegUIARDWAQ",
+    Side: "Mashed Potatoes",
+    Price: "$38.99",
+  },
+  {
+    Name: "Kanafeh",
+    IMG: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.maggwire.com%2Fwp-content%2Fuploads%2F2016%2F07%2Fkunafa-recipe.jpg&imgrefurl=https%3A%2F%2Fwww.maggwire.com%2Fkunafa-recipe%2F&tbnid=0Y4Otx9C6Yko8M&vet=12ahUKEwissv_F7oj8AhW7sHIEHU4eCLwQMygFegUIARDPAQ..i&docid=S1RigwrSpPKG9M&w=640&h=480&q=kanafeh%20professional&ved=2ahUKEwissv_F7oj8AhW7sHIEHU4eCLwQMygFegUIARDPAQ",
+    Side: "Rose Syrup",
+    Price: "$11.99",
+  },
+  {
+    Name: "Crepe Cake Slice",
+    IMG: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1921%2F3233%2Farticles%2FSUNCORE_FOODS_RAINBOW_CREPE_CAKE_800X800_1daf080f-bac5-4b60-b462-89c8d768f705.jpg%3Fv%3D1613721338&imgrefurl=https%3A%2F%2Fsuncorefoods.com%2Fblogs%2Frecipes%2Frainbow-crepe-cake&tbnid=ojDZMXGew1rIxM&vet=12ahUKEwj84IT_7oj8AhV_s3IEHRVnBrsQMygPegUIARD3AQ..i&docid=zsHg6JX-GjKg5M&w=800&h=800&q=crepe%20cake%20professional&ved=2ahUKEwj84IT_7oj8AhV_s3IEHRVnBrsQMygPegUIARD3A",
+    Side: "Fruit Serving",
+    Price: "$5.99",
+  },
+  {
+    Name: "Ice Cream",
+    IMG: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.shutterstock.com%2Fblog%2Fwp-content%2Fuploads%2Fsites%2F5%2F2017%2F04%2Fshutterstock_276826247.jpg%3Fw%3D1024%26h%3D1536&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fblog%2Fice-cream-photo-tips&tbnid=K2gNsUBIPoFZEM&vet=12ahUKEwjiscTa7oj8AhXokXIEHbvUCoIQMygIegUIARDVAQ..i&docid=Mp3ml8_XJVM_jM&w=1024&h=1536&q=ice%20cream%20professional&ved=2ahUKEwjiscTa7oj8AhXokXIEHbvUCoIQMygIegUIARDVAQ",
+    Side: "Chocolate dip + Toppings",
+    Price: "$4.99",
+  },
+];
+const DOM = {
+  mainpage: document.getElementById("menupage"),
+  dessertbtn: document.getElementById("dessert"),
+  starterbtn: document.getElementById("appetizer"),
+  maindishbtn: document.getElementById("entree"),
+  everythingbtn: document.getElementById("food"),
+  changetheme: document.getElementById("themebtn"),
+};
+
+Menu.forEach((menu) =>
+  DOM.mainpage.insertAdjacentHTML(
+    "beforeend",
+    `
+<div class="child">
+<img class="img" src="${menu.IMG}"><img>
+<h1>${menu.Name}</h1>
+<h2 class="price">${menu.Price}</h2>
+</div>
+`
+  )
+);
+
+DOM.dessertbtn.addEventListener("click", function () {
+  DOM.mainpage.innerHTML = "";
+  DessertMenu();
+});
+
+function DessertMenu() {
+  Menu.filter((menu) => menu.Type === "Dessert").forEach((menu) =>
+    DOM.mainpage.insertAdjacentHTML(
+      "beforeend",
+      `
+<div class="child">
+<img class="img" src="${menu.IMG}"><img>
+<h1>${menu.Name}</h1>
+<h2 class="price">$${menu.Price}</h2>
+</div>
+`
+    )
+  );
+}
+
+setupCounter(document.querySelector("#counter"));
+
+DOM.changetheme.addEventListener("click", function () {
+  if (document.body.classList.contains("warm")) {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  } else {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  }
+});
